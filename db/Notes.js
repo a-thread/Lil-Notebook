e1a17c4030e500fde7a9283422087003f621de6a
a-thread <aiden.threadgoode@gmail.com>
2020-09-02 15:19:29 -0400

    updating comments

--- db/Notes.js
+++ db/Notes.js
@@ -8,28 +8,28 @@ const writeFileAsync = util.promisify(fs.writeFile); // writing note
 
 // 'Notes' Constructor
 class Notes {
-
     // read note function
     read() {
         return readFileAsync("db/db.json", "utf-8")
     };
-
     // write note function
     write(note) {
         return writeFileAsync("db/db.json", JSON.stringify(note))
     };
-
     // get notes function
     getNotes() {
         return this.read().then((notes) => {
+
             // naming array
             let notesArr;
+
             // concating parsed notes into array
             try {
                 notesArr = [].concat(JSON.parse(notes))
 
+            // otherwise returning an empty array
             } catch (err) {
-                notesArr = []; // otherwise return an empty array
+                notesArr = [];
             }
             // return array
             return notesArr;
@@ -38,7 +38,7 @@ class Notes {
 
     // Add New Note Function
     addNote(note) {
-        // with the title and text
+        // note with the title and text
         const { title, text } = note;
 
         // title and text must have content to save
@@ -46,26 +46,27 @@ class Notes {
             throw new Error("Note cannot be blank!")
         };
 
-        // new note object with title, text, & id (using uuid version 4)
+        // new note object includes title, text, & id (using uuid version 4 that automatically generates unique ID)
         const newNote = { title, text, id: uuidv4() };
 
         // returning notesArr from getNotes() along with note we want to add
         return this.getNotes()
-            // expanding notesArr called "notes" and adds our new note to the end
-            .then((notes) => [...notes, newNote])
+            // expanding notesArr and adding our new note to the end
+            .then((notesArr) => [...notesArr, newNote])
             // creating a NEW ARRAY called "updatedNotes" with the new note included and writing said array
-            .then((updatedNotes) => this.write(updatedNotes))
+            .then((newNotesArr) => this.write(newNotesArr))
             // emptying newNote for the next time we need to add a note
             .then(() => newNote);
     };
 
     // "Delete" note function (done by id number)
     deleteNote(id) {
-        // This uses the same kind of logic. It doesn't actually delete the note, it creates a NEW ARRAY after filtering that note out of the array by id number.
-        return this
-            .getNotes()
-            .then(notes => notes.filter((note) => note.id !== id))
-            .then(filteredNotes => this.write(filteredNotes))
+        // This uses the same kind of logic. It doesn't actually delete the note...
+        return this.getNotes()
+            // we find the note we want to delete by filtering throught notesArr by id 
+            .then(notesArr => notesArr.filter((note) => note.id !== id))
+            // and create a NEW ARRAY with the new note inside
+            .then(filteredNotesArr => this.write(filteredNotesArr))
     }
 };
 
